6197b069eb1b07121a9cfbb02077d7bd23203d66
Ahmad Ibraheem <iba1sf@bosch.com>
2026-05-28 08:02:50 +0300

    chore: remove legacy static message preview skins

--- src/features/settings/index.ts
+++ src/features/settings/index.ts
@@ -6,7 +6,5 @@ export type {
 export { SettingsEmbeddedTabbedSkin } from './skins/embedded/SettingsEmbeddedTabbedSkin'
 export { AutomationSettingsEmbeddedSkin } from './skins/embedded/AutomationSettingsEmbeddedSkin'
 export { AutomationSettingsStandaloneSkin } from './skins/standalone/AutomationSettingsStandaloneSkin'
-export { MessagePreviewEmbeddedSkin } from './skins/embedded/MessagePreviewEmbeddedSkin'
-export { MessagePreviewStandaloneSkin } from './skins/standalone/MessagePreviewStandaloneSkin'
 export { SettingsEmbeddedSkin } from './skins/embedded/SettingsEmbeddedSkin'
 export { SettingsStandaloneSkin } from './skins/standalone/SettingsStandaloneSkin'
